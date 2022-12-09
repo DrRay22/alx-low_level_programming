6388613d540dc365768b61304d76b5cf3d652e92
“DrRay22” <scengltd@gmail.com>
2022-12-08 19:44:03 -0800

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - Entry
+ * Return: Always 0
  */
-
-int main (void)
+int main(void)
 {
 	printf("size of a char: %lu byte(s)\n", sizeof(char));
 	printf("size of an int: %lu byte(s)\n", sizeof(int));
